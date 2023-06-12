e5b7d41a1aa4f89c8f297613310d7ef90e37d0f5
sungm1nn <sungmin@DESKTOP-UN2TGEL>
2023-06-12 16:38:43 +0900

    um

--- gnews/account/urls.py
+++ gnews/account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = 'account'
+app_name = 'accounts'
 
 urlpatterns= [
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
